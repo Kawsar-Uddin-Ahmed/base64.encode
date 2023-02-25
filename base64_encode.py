207931e018af8b577d537f6a285c90e638e9e51a
Kawsar Uddin Ahmed <52331860+Kawsar-Uddin-Ahmed@users.noreply.github.com>
2023-02-26 02:34:14 +0600

    Update base64_encode.py

--- base64_encode.py
+++ base64_encode.py
@@ -7,12 +7,12 @@ with open("password.txt") as f:
        print(lines)
        for i in range(0,len(lines)):
            # print(lines[i])
-           sample_string_bytes = lines[i].encode("ascii")
+           sample_string_bytes = lines[i].encode("utf-8") #You can use "ascii" instead of "utf-8" but sometimes it may give error
            b64_bytes = base64.b64encode(sample_string_bytes)
-           b64_strings = b64_bytes.decode("ascii")
+           b64_strings = b64_bytes.decode("utf-8")
            #print(b64_strings)
            c.append(b64_strings)
-           with open("base64_password1.txt", "w") as fi:
+           with open("base64_password.txt", "w") as fi:
                print(('\n'.join(str(j) for j in c)),file=fi)
 
 
