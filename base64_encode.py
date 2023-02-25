fe8141d096ccdf1090578a6419810fed10ae0dd7
Kawsar Uddin Ahmed <52331860+Kawsar-Uddin-Ahmed@users.noreply.github.com>
2023-02-25 23:02:20 +0600

    Add files via upload

---
+++ base64_encode.py
@@ -0,0 +1,27 @@
+import base64
+
+c = []
+with open("password.txt") as f:
+       data = f.read()
+       lines = data.splitlines()
+       print(lines)
+       for i in range(0,len(lines)):
+           # print(lines[i])
+           sample_string_bytes = lines[i].encode("ascii")
+           b64_bytes = base64.b64encode(sample_string_bytes)
+           b64_strings = b64_bytes.decode("ascii")
+           #print(b64_strings)
+           c.append(b64_strings)
+           with open("base64_password.txt", "w") as fi:
+               print(('\n'.join(str(j) for j in c)),file=fi)
+
+
+
+
+
+
+
+
+
+
+
